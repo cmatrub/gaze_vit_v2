b9c7b2d7669becd1fc8b564de5a72c227a9f6d80
nityakashyap7 <nityakas@usc.edu>
2026-02-08 23:48:10 +0000

    Trainer shouldnt define loss functions

--- dev/train_vit.py
+++ dev/train_vit.py
@@ -9,23 +9,9 @@ class Trainer:
     def __init__(self, config):
         pass
 
-    def _ce_only(self, action_preds, action_targs, **kwargs):
-        return F.cross_entropy(action_preds, action_targs)
-
-    def _ce_plus_gaze_reg(self, action_preds, action_targs, gaze_preds, gaze_targs, reg_lambda=1.0, **kwargs):
-        ''' averages over batch_size and num_heads in one go. if u wanna see the intermediary ce values for each head separately u need to define a separate loss function'''
-        ce = self._ce_only(action_preds, action_targs)
-
-        #  collapse the batch_size and num_heads dim into one that cross_entropy will average over to return u a scalar (this averages across the batch and heads so u dont need to do 2 separate averages)
-        gaze_preds = rearrange(gaze_preds, 'b h n -> (b h) n')
-        gaze_targs = rearrange(gaze_targs, 'b h n -> (b h) n')
-        reg = reg_lambda * F.cross_entropy(gaze_preds, gaze_targs)
-
-        return ce + reg
-
 
     def _train_step(self, loss_fn):
-        
+
         pass
 
     def train(self):
