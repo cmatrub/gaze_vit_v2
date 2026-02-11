3ad1dbcd179bb7aba83e0e014e38e78bfee66ff5
nityakashyap7 <nityakas@usc.edu>
2026-02-11 03:15:36 +0000

    AAAAAA

---
+++ dev/losses/ce_plus_gaze_reg.py
@@ -0,0 +1,13 @@
+import torch.nn.functional as F
+
+class CEPlusGazeReg: 
+    def __init__(self, reg_lambda=1.0):
+        self.reg_lambda = reg_lambda
+
+
+    def __call__(self, action_preds, action_targs, gaze_preds, gaze_targs, **kwargs):
+        ce = F.cross_entropy(action_preds, action_targs)
+
+        reg = self.reg_lambda * F.cross_entropy(gaze_preds, gaze_targs)
+
+        return ce + reg
